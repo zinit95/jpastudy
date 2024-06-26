f018a9b699a595518a3030b240da822d3ce34d7b
jzinit <kyj32323@naver.com>
2024-06-26 11:24:30 +0900

    # 9. JPA Lazy loding

--- src/main/java/com/spring/jpastudy/chap04_relation/entity/Employee.java
+++ src/main/java/com/spring/jpastudy/chap04_relation/entity/Employee.java
@@ -5,7 +5,7 @@ import lombok.*;
 import javax.persistence.*;
 
 @Setter @Getter
-@ToString
+@ToString(exclude =  "department") // 연관 관계 필든는 제외
 @EqualsAndHashCode(of = "id")
 @NoArgsConstructor
 @AllArgsConstructor
@@ -23,13 +23,25 @@ public class Employee {
     private String name;
 
     // 단방향 매핑 - 데이터베이스처럼 한쪽에 상대방의 PK를 FK로 갖는 형태
-    @ManyToOne
+    //EAGER Loading : 연관된 데이터를 항상 join 을 통해 가져옴
+    //LAZY Loading : 해당 엔터티만 데이터만 가져오고 필요한 경우 연관 엔터티를 가져옴
+    @ManyToOne(fetch = FetchType.EAGER) // 어려우면 LAZY 로 쓰라
     @JoinColumn(name = "dept_id")  // FK 컬럼명
     private Department department;
 
 //    @ManyToOne
 //    @JoinColumn(name = "receive_dept_id")
 //    private Department department2;
+
+
+//    @Override
+//    public String toString() {
+//        return "Employee{" +
+//                "id=" + id +
+//                ", name='" + name + '\'' +
+//                //", department=" + department +
+//                '}';
+//    }
 }
 
 
--- src/test/java/com/spring/jpastudy/chap04_relation/repository/EmployeeRepositoryTest.java
+++ src/test/java/com/spring/jpastudy/chap04_relation/repository/EmployeeRepositoryTest.java
@@ -82,6 +82,10 @@ class EmployeeRepositoryTest {
         System.out.println("\n\n\n");
         System.out.println("employee = " + employee);
         System.out.println("\n\n\n");
+
+        Department department = employee.getDepartment();
+        System.out.println("department = " + department);
+
     }
 
 
