4feb8ff83a29c41fe17dd8bf21c13c31c1710358
jzinit <kyj32323@naver.com>
2024-06-27 09:46:59 +0900

    수정

--- src/main/java/com/spring/jpastudy/chap04_relation/entity/Employee.java
+++ src/main/java/com/spring/jpastudy/chap04_relation/entity/Employee.java
@@ -25,7 +25,7 @@ public class Employee {
     // 단방향 매핑 - 데이터베이스처럼 한쪽에 상대방의 PK를 FK로 갖는 형태
     //EAGER Loading : 연관된 데이터를 항상 join 을 통해 가져옴
     //LAZY Loading : 해당 엔터티만 데이터만 가져오고 필요한 경우 연관 엔터티를 가져옴
-    @ManyToOne(fetch = FetchType.EAGER) // 어려우면 LAZY 로 쓰라
+    @ManyToOne(fetch = FetchType.LAZY) // 어려우면 LAZY 로 쓰라
     @JoinColumn(name = "dept_id")  // FK 컬럼명
     private Department department;
 
