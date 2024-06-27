0ddf03096366bdb5ff61f179e07508c7575a03fb
jzinit <kyj32323@naver.com>
2024-06-27 10:54:49 +0900

    # 15. 다대다 관계 시나리오 테스트

---
+++ src/main/java/com/spring/jpastudy/chap05/repository/PurchaseRepository.java
@@ -0,0 +1,8 @@
+package com.spring.jpastudy.chap05.repository;
+
+import com.spring.jpastudy.chap05.entity.Purchase;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PurchaseRepository extends JpaRepository<Purchase, Long> {
+
+}
